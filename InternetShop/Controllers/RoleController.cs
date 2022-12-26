f8d70f183ebd9fa9c49fe7d0c280fccd2a4e46de
Tysyatsky <tysyatsky@gmail.com>
2022-12-26 14:48:33 +0200

    initialization

---
+++ InternetShop/Controllers/RoleController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace InternetShop.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromForm] string? name)
+        {
+            var role = await roleManager.FindByNameAsync(name);
+            if(role != null)
+            {
+                await roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Delete");
+        }
+    }
+}
