5a8d7018dc89fd9dc90694d31e8ff1edd615baa1
starodubbb <e.molodid2011@gmail.com>
2022-12-30 17:12:43 +0200

    CouchDB implementing + invoices
    1. InvoiceContent.cs
    2. Invoice.cs
    3. InvoiceFactory.cs

--- InternetShop/Data/InvoiceContext.cs
+++ InternetShop/Data/InvoiceContext.cs
@@ -1,6 +1,19 @@
-﻿namespace InternetShop.Data
+﻿using CouchDB.Driver;
+using CouchDB.Driver.Options;
+using InternetShop.Models;
+
+namespace InternetShop.Data
 {
-    public class InvoiceContext
+    public class InvoiceContext : CouchContext
     {
+        public CouchDatabase<Invoice> Invoices { get; set; }
+        protected override void OnConfiguring(CouchOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseEndpoint("http://localhost:5984/")
+                .EnsureDatabaseExists()
+                .UseBasicAuthentication("couchdb", "couchdb");
+        }
     }
+      
 }
